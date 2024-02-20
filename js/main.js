68a1a6281c0f06c349e5564683ee916d14eee1ff
Anastasia Mikhailova <fasechka_99@mail.ru>
2024-02-20 17:26:08 +0300

    add: cta-form

--- js/main.js
+++ js/main.js
@@ -90,7 +90,7 @@ const swiper = new Swiper('.features-slider', {
 
 const swiperBlog = new Swiper('.blog-slider', {
   speed: 400,
-  slidesPerView: 2,
+  slidesPerView: 1,
   spaceBetween: 30,
   navigation: {
     nextEl: '.blog-button-next',
